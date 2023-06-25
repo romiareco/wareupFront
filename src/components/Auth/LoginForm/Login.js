0ca1e2e5576f2d44df3f1eb2a143eda4ff85adca
letipresa <leticia.presa19@gmail.com>
2023-06-24 21:40:41 -0300

    Fix view for login page

--- src/components/Auth/LoginForm/Login.js
+++ src/components/Auth/LoginForm/Login.js
@@ -1,6 +1,6 @@
 import { LoadingButton } from '@mui/lab';
 import { Card, Checkbox, Grid, TextField } from '@mui/material';
-import { Box, styled } from '@mui/system';
+import { Box, styled, useTheme } from '@mui/system';
 import { Paragraph } from '../../Typography';
 import {useAuth} from '../../../hooks';
 import { NavLink, useNavigate } from 'react-router-dom';
@@ -9,6 +9,16 @@ import {validationSchema, inititalValues} from "./Login.form";
 import { Auth } from "../../../api";
 import { useState } from 'react';
 import { Form } from "semantic-ui-react";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const FlexBox = styled(Box)(() => ({ display: 'flex', alignItems: 'center' }));
 
@@ -35,6 +45,7 @@ const LoginRoot = styled(JustifyBox)(() => ({
 }));
 
 const authController = new Auth();
+const defaultTheme = createTheme();
 
 export function Login() {
   const { login } = useAuth();
@@ -63,16 +74,39 @@ export function Login() {
   });
 
   return (
-    <LoginRoot>
-      <Card className="card">
-        <Grid container>
-          <Grid item sm={6} xs={12}>
-            <JustifyBox p={4} height="100%" sx={{ minWidth: 320 }}>
-              <img src="/assets/images/illustrations/dreamer.svg" width="100%" alt="" />
-            </JustifyBox>
-          </Grid>
-
-          <Grid item sm={6} xs={12}>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+              backgroundImage: 'url(https://wareup.com.uy/wp-content/uploads/2021/07/banner-1.jpg)',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Iniciar sesión
+            </Typography>
             <ContentBox>
                   <Form onSubmit={formik.handleSubmit}>
                     <TextField
@@ -119,10 +153,10 @@ export function Login() {
                         to="/users/forgot-password"
                         style={{ color: "green" }}
                       >
-                        Olvidaste tu contraseña?
+                        ¿Olvidaste tu contraseña?
                       </NavLink>
                     </FlexBox>
-
+                    
                     <LoadingButton
                       type="submit"
                       color="primary"
@@ -141,9 +175,8 @@ export function Login() {
                     >
                       Cancelar
                     </LoadingButton>
-
                     <Paragraph>
-                      No tienes una cuenta?
+                      ¿No tienes una cuenta?
                       <NavLink
                         to="/users/register"
                         style={{ color: "green", marginLeft: 5 }}
@@ -154,9 +187,9 @@ export function Login() {
                     <p className="register-form__error">{error}</p>
                   </Form>
             </ContentBox>
-          </Grid>
+          </Box>
         </Grid>
-      </Card>
-    </LoginRoot>
+      </Grid>
+    </ThemeProvider>
   );
 }
