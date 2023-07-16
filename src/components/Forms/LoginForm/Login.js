bea2a58cecf1f442ad6fc2c629147f6b7cf723ec
letipresa <leticia.presa19@gmail.com>
2023-07-16 11:33:26 -0300

    Working on I have space page

--- src/components/Forms/LoginForm/Login.js
+++ src/components/Forms/LoginForm/Login.js
@@ -1,33 +1,23 @@
-import { LoadingButton } from '@mui/lab';
-import { Checkbox, Grid, TextField } from '@mui/material';
-import { Box, styled } from '@mui/system';
-import { Paragraph } from '../../Typography';
-import {useAuth} from '../../../hooks';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { LoadingButton } from "@mui/lab";
+import { Checkbox, Grid, TextField } from "@mui/material";
+import { Box, styled } from "@mui/system";
+import { Paragraph } from "../../Typography";
+import { useAuth } from "../../../hooks";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
-import {validationSchema, inititalValues} from "./Login.form";
+import { validationSchema, inititalValues } from "./Login.form";
 import { Auth } from "../../../api";
-import { useState } from 'react';
+import { useState } from "react";
 import { Form } from "semantic-ui-react";
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Paper from '@mui/material/Paper';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Dialog, DialogContent, DialogTitle, Button } from "@mui/material";
 
-const FlexBox = styled(Box)(() => ({ display: 'flex', alignItems: 'center' }));
-
-const ContentBox = styled(Box)(() => ({
-  height: '100%',
-  padding: '32px',
-  position: 'relative',
-  background: 'rgba(0, 0, 0, 0.01)',
-}));
-
+const FlexBox = styled(Box)(() => ({ display: "flex", alignItems: "center" }));
 const authController = new Auth();
-const defaultTheme = createTheme();
 
 export function Login() {
   const { login } = useAuth();
@@ -43,10 +33,10 @@ export function Login() {
         setError("");
         const response = await authController.login(formValue);
 
-        authController.setAccessToken(response.access);
-        authController.setRefreshToken(response.refresh);
+        authController.setAccessToken(response.tokens.access);
+        authController.setRefreshToken(response.tokens.refresh);
 
-        login(response.access);
+        login(response.tokens.access);
         navigate("/home");
       } catch (exception) {
         console.error(exception.msg);
@@ -56,122 +46,91 @@ export function Login() {
   });
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-            item
-            xs={false}
-            sm={4}
-            md={7}
-            sx={{
-              backgroundImage: 'url(https://wareup.com.uy/wp-content/uploads/2021/07/banner-1.jpg)',
-              backgroundRepeat: 'no-repeat',
-              backgroundColor: (t) =>
-                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-            }}
-          />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      BackdropProps={{
+        sx: {
+          backdropFilter: "blur(8px)",
+          backgroundColor: "rgba(255, 255, 255, 0.8)", // Ajusta el color y la opacidad del fondo desenfocado según tus necesidades
+        },
+      }}
+    >
+      <DialogTitle>Iniciar sesión</DialogTitle>
+      <DialogContent>
+        <TextField
+          fullWidth
+          size="small"
+          type="email"
+          name="email"
+          label="Email"
+          variant="outlined"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+          helperText={formik.errors.email}
+          sx={{ mb: 3 }}
+        />
+        <TextField
+          fullWidth
+          size="small"
+          name="password"
+          type="password"
+          label="Contraseña"
+          variant="outlined"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+          helperText={formik.errors.password}
+          sx={{ mb: 1.5 }}
+        />
+        <FlexBox justifyContent="space-between">
+          <FlexBox gap={1}>
+            <Checkbox
+              size="small"
+              name="remember"
+              onChange={formik.handleChange}
+              checked={formik.values.remember}
+              sx={{ padding: 0 }}
+            />
+
+            <Paragraph>Recuerdame</Paragraph>
+          </FlexBox>
+
+          <NavLink to="/users/forgot-password" style={{ color: "green" }}>
+            ¿Olvidaste tu contraseña?
+          </NavLink>
+        </FlexBox>
+
+        <LoadingButton
+          type="submit"
+          color="primary"
+          loading={formik.isSubmitting}
+          variant="contained"
+          sx={{ my: 2 }}
+        >
+          Login
+        </LoadingButton>
+
+        <LoadingButton
+          color="primary"
+          variant="outlined"
+          onClick={() => navigate("/")}
+          sx={{ my: 2, ml: 1 }}
+        >
+          Cancelar
+        </LoadingButton>
+        <Paragraph>
+          ¿No tienes una cuenta?
+          <NavLink
+            to="/users/register"
+            style={{ color: "green", marginLeft: 5 }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Iniciar sesión
-            </Typography>
-            <ContentBox>
-                  <Form onSubmit={formik.handleSubmit}>
-                    <TextField
-                      fullWidth
-                      size="small"
-                      type="email"
-                      name="email"
-                      label="Email"
-                      variant="outlined"
-                      value={formik.values.email}
-                      onChange={formik.handleChange}
-                      error={formik.errors.email}
-                      helperText={formik.errors.email}
-                      sx={{ mb: 3 }}
-                    />
-                    <TextField
-                      fullWidth
-                      size="small"
-                      name="password"
-                      type="password"
-                      label="Contraseña"
-                      variant="outlined"
-                      value={formik.values.password}
-                      onChange={formik.handleChange}
-                      error={formik.errors.password}
-                      helperText={formik.errors.password}
-                      sx={{ mb: 1.5 }}
-                    />
-
-                    <FlexBox justifyContent="space-between">
-                      <FlexBox gap={1}>
-                        <Checkbox
-                          size="small"
-                          name="remember"
-                          onChange={formik.handleChange}
-                          checked={formik.values.remember}
-                          sx={{ padding: 0 }}
-                        />
-
-                        <Paragraph>Recuerdame</Paragraph>
-                      </FlexBox>
-
-                      <NavLink
-                        to="/users/forgot-password"
-                        style={{ color: "green" }}
-                      >
-                        ¿Olvidaste tu contraseña?
-                      </NavLink>
-                    </FlexBox>
-                    
-                    <LoadingButton
-                      type="submit"
-                      color="primary"
-                      loading={formik.isSubmitting}
-                      variant="contained"
-                      sx={{ my: 2 }}
-                    >
-                      Login
-                    </LoadingButton>
-
-                    <LoadingButton
-                      color="primary"
-                      variant="outlined"
-                      onClick={() => navigate("/")}
-                      sx={{ my: 2, ml: 1 }}
-                    >
-                      Cancelar
-                    </LoadingButton>
-                    <Paragraph>
-                      ¿No tienes una cuenta?
-                      <NavLink
-                        to="/users/register"
-                        style={{ color: "green", marginLeft: 5 }}
-                      > 
-                        Registrarse
-                      </NavLink>
-                    </Paragraph>
-                    <p className="register-form__error">{error}</p>
-                  </Form>
-            </ContentBox>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+            Registrarse
+          </NavLink>
+        </Paragraph>
+        <p className="register-form__error">{error}</p>
+      </DialogContent>
+    </Dialog>
   );
 }
--- src/components/Home/TopHomeBar/TopHomeBar.js
+++ src/components/Home/TopHomeBar/TopHomeBar.js
@@ -1,68 +1,34 @@
-import { Box, styled } from '@mui/system';
-import {topBarHeight } from "../../../utils/constant";
-import { MenuItem } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { Span } from '../../Typography';
+import { Box, styled } from "@mui/system";
+import { topBarHeight } from "../../../utils/constant";
+import { MenuItem } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Span } from "../../Typography";
+import React from "react";
+import { AppBar, Toolbar, Button } from "@mui/material";
 
+const Topbar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: "#007bff", // Cambia este color a tu tono azul elegante deseado
+}));
 
-export function TopHomeBar() {  
-    const TopbarRoot = styled('div')(({ theme }) => ({
-        top: 0,
-        zIndex: 96,
-        transition: 'all 0.3s ease',
-        height: topBarHeight,
-      }));
-    
-    const TopbarContainer = styled(Box)(({ theme }) => ({
-    padding: '8px',
-    paddingLeft: 18,
-    paddingRight: 20,
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    background: "black",
-    [theme.breakpoints.down('sm')]: {
-        paddingLeft: 16,
-        paddingRight: 16,
-    },
-    [theme.breakpoints.down('xs')]: {
-        paddingLeft: 14,
-        paddingRight: 16,
-    },
-    }));
-
-    const StyledItem = styled(MenuItem)(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    minWidth: 185,
-    '& a': {
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        textDecoration: 'none',
-    },
-    '& span': { marginRight: '10px', color: "green" },
-    }));
-
-    return (
-         /*Deberia fijar la bar pero no se fija... hay que revisar porque*/
-        <TopbarRoot position="fixed">
-            <TopbarContainer>
-                <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-                    <StyledItem position="center">
-                        <Link to="/users/login">
-                            <Span> Login </Span>
-                        </Link>
-                    </StyledItem>
-                    <StyledItem>
-                        <Link to="/users/register">
-                            <Span> Registrarse </Span>
-                        </Link>
-                    </StyledItem>
-                </Box>
-            </TopbarContainer>
-        </TopbarRoot> 
-    );
-  }
-  
+export function TopHomeBar() {
+  return (
+    <Topbar position="fixed">
+      <Toolbar sx={{ justifyContent: "flex-end" }}>
+        <Button
+          variant="outlined"
+          color="inherit"
+          sx={{ color: "#ffffff", borderColor: "#ffffff", mr: 1 }}
+        >
+          Iniciar sesión
+        </Button>
+        <Button
+          variant="outlined"
+          color="inherit"
+          sx={{ color: "#ffffff", borderColor: "#ffffff" }}
+        >
+          Registrarse
+        </Button>
+      </Toolbar>
+    </Topbar>
+  );
+}
