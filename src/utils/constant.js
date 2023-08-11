a78bd1fcd9259f268b19bba3923df6a4aec8c7e9
letipresa <leticia.presa19@gmail.com>
2023-08-11 12:23:20 -0300

    Merge with main

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -22,11 +22,7 @@ export const ENV = {
         CONTACT: "users/contact",
         UPDATE_PASSWORD: "users/update-password",
         DEPARTMENTS: "common/departments",
-<<<<<<< HEAD
-        DEPOSITS: "deposits",
-=======
         STORAGE: "deposits"
->>>>>>> main
     },
     JWT: {
         ACCESS: "access",
