9fccd5d14c3ed9ef319edf9d728c20a9c09dc5d8
letipresa <leticia.presa19@gmail.com>
2023-09-03 11:52:59 -0300

    Delete comments and add footer

--- src/pages/publication/PublicationView.js
+++ src/pages/publication/PublicationView.js
@@ -1,31 +1,33 @@
-import { Container, Divider, Stack } from "@mui/material";
+import { Container, Divider } from "@mui/material";
 import { Gallery } from "./Gallery";
 import { Description } from "./Description";
 import { Services } from "./Services";
-import React, { useState, useEffect } from "react";
+import { Footer } from "../../components/Footer";
+import React from "react";
 import "../../theme/PublicationView.css";
 
 import { Box } from "@mui/system";
-import { useAuth } from "../../hooks";
 import { useLocation } from "react-router-dom";
 
 export function PublicationView() {
   const location = useLocation();
 
-  // Obtén el parámetro depositId de la URL
   const searchParams = new URLSearchParams(location.search);
   const depositId = searchParams.get("id");
 
   return (
-    <Container component="section" maxWidth={"lg"}>
-      <Box className="core">
-        <Gallery depositId={depositId} />
-        <Description depositId={depositId} />
-      </Box>
-      <Divider />
-      <Box className="core">
-        <Services depositId={depositId} />
-      </Box>
-    </Container>
+    <Box>
+      <Container component="section" maxWidth={"lg"}>
+        <Box className="core">
+          <Gallery depositId={depositId} />
+          <Description depositId={depositId} />
+        </Box>
+        <Divider />
+        <Box className="core">
+          <Services depositId={depositId} />
+        </Box>
+      </Container>
+      <Footer />
+    </Box>
   );
 }
--- src/pages/publication/Services.js
+++ src/pages/publication/Services.js
@@ -1,15 +1,28 @@
 import { useAuth } from "../../hooks";
 import React, { useEffect, useState } from "react";
-import { Deposit } from "../../api";
+import { Deposit, Service } from "../../api";
 import { Typography, FormGroup, Box, Stack } from "@mui/material";
 import MiscellaneousServicesRoundedIcon from "@mui/icons-material/MiscellaneousServicesRounded";
 import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
 
 const depositController = new Deposit();
+const serviceController = new Service();
 
 export function Services({ depositId }) {
   const [depositServices, setDepositServices] = useState([]);
+  const [serviceGroups, setServiceGroups] = useState([]);
   const { accessToken } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await serviceController.getAllServices(accessToken);
+        setServiceGroups(response.serviceGroups);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [accessToken, depositId]);
 
   useEffect(() => {
     (async () => {
@@ -27,32 +40,53 @@ export function Services({ depositId }) {
 
   return (
     <Box>
-      <Typography variant="h6">
-        <MiscellaneousServicesRoundedIcon />
+      <Typography variant="h4">
+        <MiscellaneousServicesRoundedIcon
+          style={{ fontSize: "2rem", marginRight: "8px" }}
+        />
         Servicios disponibles
       </Typography>
-      <Box>
-        {depositServices &&
-          depositServices.map((group) => (
-            <div key={group.id}>
-              <Typography variant="h6">{group.title}</Typography>
-              <FormGroup>
-                <Stack direction="row">
-                  {group.services.map((service) => (
-                    <div
-                      key={service.id}
-                      style={{ display: "flex", alignItems: "center" }}
-                    >
-                      <CheckRoundedIcon style={{ marginRight: "8px" }} />{" "}
-                      {/* Icono */}
-                      <Typography>{service.title}</Typography>{" "}
-                      {/* Label del servicio */}
-                    </div>
-                  ))}
-                </Stack>
-              </FormGroup>
-            </div>
-          ))}
+      <Box padding={2}>
+        {serviceGroups &&
+          serviceGroups.map((group) => {
+            // Verificar si depositServices no es null antes de filtrar
+            const groupDepositServices =
+              depositServices &&
+              depositServices.filter(
+                (myService) => myService.service.serviceGroupId === group.id
+              );
+
+            // Verificar si hay depositServices para este group
+            if (groupDepositServices && groupDepositServices.length > 0) {
+              return (
+                <div key={group.id}>
+                  <Typography variant="h5">{group.title}</Typography>
+                  <FormGroup>
+                    <Stack direction="row" padding={2}>
+                      {groupDepositServices.map((myService) => (
+                        <div
+                          key={myService.service.id}
+                          style={{ display: "flex", alignItems: "center" }}
+                        >
+                          <CheckRoundedIcon
+                            style={{ marginRight: "1px", marginLeft: "20px" }}
+                          />{" "}
+                          {/* Icono */}
+                          <Typography>
+                            {myService.service.title}
+                          </Typography>{" "}
+                          {/* Label del servicio */}
+                        </div>
+                      ))}
+                    </Stack>
+                  </FormGroup>
+                </div>
+              );
+            }
+
+            // Si no hay depositServices para este group, no se renderiza nada
+            return null;
+          })}
       </Box>
     </Box>
   );
