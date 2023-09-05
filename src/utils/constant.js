2a2c541cc3840ac713a99de70a43f97a959729f6
Romina Areco <romina.areco@hotmail.com>
2023-09-04 23:03:51 -0300

    Config

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,12 +4,10 @@ export const navbarHeight = 60
 export const sidenavCompactWidth = 80
 export const containedLayoutWidth = 1200
 
-const SERVER_IP = "localhost:3001";
-
 //TODO: separar API_ROUTES por entidad
 export const ENV = {
-    BASE_PATH: `http://${SERVER_IP}`,
-    BASE_API: `http://${SERVER_IP}/api/v1`,
+    BASE_PATH: `http://${process.env.REACT_APP_BASE_PATH}`,
+    BASE_API: `http://${process.env.REACT_APP_BASE_PATH}/api/v1`,
     API_ROUTES: {
         LOGIN: "auth/login",
         REFRESH_ACCESS_TOKEN: "auth/refresh_access_token",
