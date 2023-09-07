44a2489cd63ad3d21da38268e6399d5aa9f31dc7
Romina Areco <romina.areco@hotmail.com>
2023-09-06 21:49:34 -0300

    Rutas

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,14 +4,10 @@ export const navbarHeight = 60
 export const sidenavCompactWidth = 80
 export const containedLayoutWidth = 1200
 
-const SERVER_IP = process.env.BASE_PATH;
-
-console.log(process.env.BASE_PATH); 
-
 //TODO: separar API_ROUTES por entidad
 export const ENV = {
-    BASE_PATH: process.env.BASE_PATH,
-    BASE_API: process.env.BASE_PATH+"api/v1",
+    BASE_PATH: "https://wareup-api.onrender.com",
+    BASE_API: "https://wareup-api.onrender.com/api/v1",
     API_ROUTES: {
         LOGIN: "auth/login",
         REFRESH_ACCESS_TOKEN: "auth/refresh_access_token",
