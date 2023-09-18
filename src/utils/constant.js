abef4ffb4a884d14d3d4787f4d44275171f21641
Romina Areco <romina.areco@hotmail.com>
2023-09-18 20:55:54 -0300

    Fix api

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,11 +4,11 @@ export const navbarHeight = 60
 export const sidenavCompactWidth = 80
 export const containedLayoutWidth = 1200
 
-const SERVER_IP = "localhost:3001";
+const SERVER_IP = "wareup-api-dev.onrender.com";
 //TODO: separar API_ROUTES por entidad
-export const ENV = {
-    BASE_PATH: `http://${SERVER_IP}`,
-    BASE_API: `http://${SERVER_IP}/api/v1`,
+export const ENV = { 
+    BASE_PATH: `https://${SERVER_IP}`,
+    BASE_API: `https://${SERVER_IP}/api/v1`, 
     API_ROUTES: {
         LOGIN: "auth/login",
         REFRESH_ACCESS_TOKEN: "auth/refresh_access_token",
